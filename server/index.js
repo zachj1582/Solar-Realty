d60bd2763a3b4e22256678d2905d6b739a73bc3f
Zachary Johnson <zach_j@att.net>
2020-03-05 16:29:43 -0700

    again

--- server/index.js
+++ server/index.js
@@ -19,38 +19,39 @@ app.use(
         saveUninitialized: false,
         secret: SESSION_SECRET
     })
-)
-massive(CONNECTION_STRING).then(db => {
-    app.set('db', db)
-    console.log('DB conncected')
+    )
+    massive(CONNECTION_STRING).then(db => {
+        app.set('db', db)
+        console.log('DB conncected')
+    })
+    
+    
+    //endpoints
+    //auth
+    app.post('/auth/register', ac.register)
+    app.post('/auth/login', ac.login)
+    app.post('/auth/logout', ac.logout)
+    // app.get('/auth/user', ac.getUser)
+    //products
+    app.get('/api/cart/:id', pc.getCart)
+    app.post('/api/cart', pc.addToCart)
+    app.get('/api/products', pc.getProducts)
+    app.delete('/api/cart/:id', pc.removeItem)
+    app.get('/api/product/:id', pc.getProduct)
+    app.delete('/api/product/:id', pc.soldItem)
+    app.post('/api/product', pc.addProperty)
+    app.put('/api/product/:id', pc.editProperty)
+    //admin
+    app.post('/auth/adminregister', adminc.registerAdmin)
+    app.post('/auth/adminlogin', adminc.loginAdmin)
+    app.post('/auth/adminlogout', adminc.logoutAdmin)
+    app.get('/auth/admin', adminc.getAdmin)
+    // stripe
+    app.post('/api/payment', sc.makePayment)
+    
+    app.use(express.static(__dirname + '/../build'))
+    app.get('*', (req,res)=> {
+        res.sendFile(path.join(__dirname, '../build/index.html'))
+    })
+    
     app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
-})
-
-
-//endpoints
-//auth
-app.post('/auth/register', ac.register)
-app.post('/auth/login', ac.login)
-app.post('/auth/logout', ac.logout)
-// app.get('/auth/user', ac.getUser)
-//products
-app.get('/api/cart/:id', pc.getCart)
-app.post('/api/cart', pc.addToCart)
-app.get('/api/products', pc.getProducts)
-app.delete('/api/cart/:id', pc.removeItem)
-app.get('/api/product/:id', pc.getProduct)
-app.delete('/api/product/:id', pc.soldItem)
-app.post('/api/product', pc.addProperty)
-app.put('/api/product/:id', pc.editProperty)
-//admin
-app.post('/auth/adminregister', adminc.registerAdmin)
-app.post('/auth/adminlogin', adminc.loginAdmin)
-app.post('/auth/adminlogout', adminc.logoutAdmin)
-app.get('/auth/admin', adminc.getAdmin)
-// stripe
-app.post('/api/payment', sc.makePayment)
-
-app.use(express.static(__dirname + '/../build'))
-app.get('*', (req,res)=> {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
-})
